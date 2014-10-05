bb19d51a73cb189b572f54b96ea7b75305996667
wfredlamb <wilfredlamb@Wilsons-MacBook-Pro.local>
2014-10-04 21:11:27 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Wilson Lam on 10/4/14.
+//  Copyright (c) 2014 Wilson Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITableViewController Challenge Solution/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Wilson Lam on 10/4/14.
+//  Copyright (c) 2014 Wilson Lam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
