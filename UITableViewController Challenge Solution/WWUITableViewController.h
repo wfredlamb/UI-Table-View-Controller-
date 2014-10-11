5f31d5a036243bd009c862f2cb94a3f8fdf09dd5
wfredlamb <wilfredlamb@Wilsons-MacBook-Pro.local>
2014-10-11 12:29:56 -0400

    solved

--- UITableViewController Challenge Solution/WWUITableViewController.h
+++ UITableViewController Challenge Solution/WWUITableViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  UITableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Wilson Lam on 10/4/14.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface WWUITableViewController : UITableViewController
 
 @end
-
